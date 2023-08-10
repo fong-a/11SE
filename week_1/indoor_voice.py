6dd06cb5ca2c59064afa4264a83129ac42c4cce8
Mr Fong <fong.a@saintaug.nsw.edu.au>
2023-08-10 13:33:54 +1000

    Added indoor_voice

---
+++ week_1/indoor_voice.py
@@ -0,0 +1,19 @@
+"""
+INDOOR VOICE
+------------
+
+WRITING IN ALL CAPS IS LIKE YELLING.
+
+Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+
+TASK:
+In this file, implement a program in Python that prompts the user for input and then outputs that same input in lowercase. 
+Punctuation and whitespace should be outputted unchanged. 
+You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.
+
+HINTS:
+- Recall that the str data types comes with quite a few methods, per docs.python.org/3/library/stdtypes.html#string-methods.
+
+"""
+
+# TODO: Write your code below
